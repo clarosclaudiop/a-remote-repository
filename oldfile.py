23be3679a2ed421bd777ff7e59e90149f2428b1b
clarosclaudiop <clarosclaudiop@gmail.com>
2024-03-14 22:38:39 -0400

    Se agrega una respuesta a la question con esta commit

--- oldfile.py
+++ oldfile.py
@@ -7,4 +7,10 @@
 # Yei8
 # Changing one line at one file, has the effect: 1 insertion(+), 1 deletion(-), 1 file changes
 # SIETE
-# Commits are integrated into the git repository after fetch? Or not really, and merge does that.
+# Commits are copied into the local git repository after fetch? Or not really, and merge does that.
+    # Pues git fetch sí descarga todos commits que no tenías del repositorio remoto, de una forma tal que aparecen al hacer git log --all --oneline --graph.
+    # Es decir, esas commits del remoto sí quedan dentro del repositorio luego de fetch.    
+    # Mira, esto aparece:
+    #* dce5a31 (origin/main) Se agregó una question con esta commit.
+    #* c1d4853 (HEAD -> main) Se renombró newfile.py to oldfile.py.
+    # Merge fusiona branches, es decir, las commits apuntadas.
