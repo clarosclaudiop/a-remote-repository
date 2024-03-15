dce5a3155573a01f73fc4e99e1845055976329a0
clarosclaudiop <118923272+clarosclaudiop@users.noreply.github.com>
2024-03-14 22:19:51 -0400

    Se agregó una question con esta commit.

--- oldfile.py
+++ oldfile.py
@@ -7,3 +7,4 @@
 # Yei8
 # Changing one line at one file, has the effect: 1 insertion(+), 1 deletion(-), 1 file changes
 # SIETE
+# Commits are integrated into the git repository after fetch? Or not really, and merge does that.
